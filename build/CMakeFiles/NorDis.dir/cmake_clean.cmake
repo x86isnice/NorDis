d96570127d7c1221c437a92b779173ba64d6499a
x86isnice <x86isnice@gmail.com>
2016-04-26 15:17:07 +0800

    changes log

---
+++ build/CMakeFiles/NorDis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NorDis.dir/NorDis.cpp.o"
+  "NorDis.pdb"
+  "NorDis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NorDis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
